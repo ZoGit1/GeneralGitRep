2f08c7739aef15ebad2843d5a193c86bdbd307c9
ZoGit1 <sorenekon@yahoo.com>
2017-05-10 11:07:41 +0200

    First commit.

---
+++ Untitled.m
@@ -0,0 +1,19 @@
+% !git init
+% !git add -A
+% !git commit -m "First commit.
+%!git remote add origin https://github.com/ZoGit1/GeneralGitRep.git "
+% !git push -u origin master
+% 
+
+% if problems:
+%!git branch --set-upstream-to=origin/master
+%!git merge --allow-unrelated-histories
+
+% Once off
+% !git config --global user.name ZoGit1
+% !git config --global user.email sorenekon@yahoo.com
+%  https://github.com/ZoGit1/GeneralGitRep.git
+
+
+
+zkjhvkzhzxcb
